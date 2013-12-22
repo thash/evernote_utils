21302a8a8170f0258fde906072c85cfefd456262
memerelics <takuya21hashimoto@gmail.com>
2013-12-23 02:01:28 +0900

    additional notes search options (words, order, and ascending)

--- lib/evernote_utils/note.rb
+++ lib/evernote_utils/note.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/object'
+
 module ENUtils
   class Note < Evernote::EDAM::Type::Note
 
@@ -16,6 +18,11 @@ module ENUtils
     #   attributes:<Evernote::EDAM::Type::NoteAttributes >
 
     attr_reader :guid, :title, :contentHash, :contentLength, :created, :updated, :active, :updateSequenceNum, :notebookGuid, :attributes
+
+    # created: 1, updated: 2, relevance: 3, update_sequence_number: 4, title: 5
+    OrderFields = Evernote::EDAM::Type::NoteSortOrder::VALUE_MAP.reduce({}){|accum, pair|
+      accum.merge(pair.last.downcase.to_sym => pair.first)
+    }
 
     def initialize(edam_note)
        @guid              = edam_note.guid
@@ -49,7 +56,16 @@ module ENUtils
         tag_guids = (tags || [tag]).map{|t| t.is_a?(ENUtils::Tag) ? t.guid : t }
         filter.tagGuids = tag_guids
       end
+      filter.words     = options[:words] if options[:words]
+      filter.order     = OrderFields[options[:order].to_sym] if available_order?(options[:order])
+      filter.ascending = options[:asc] if options[:asc]
       filter
     end
+
+    def self.available_order?(value)
+      return false if value.nil?
+      OrderFields.keys.include?(value.to_sym)
+    end
+
   end
 end
