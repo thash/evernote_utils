7a4d46da60011267a3f8d838f4d5968171d0e982
memerelics <takuya21hashimoto@gmail.com>
2013-12-23 04:57:49 +0900

    cache total_count

--- lib/evernote_utils/array.rb
+++ lib/evernote_utils/array.rb
@@ -11,11 +11,15 @@ module ENUtils
       super(array)
     end
 
+    def total_count
+      @total_count ||= get_total_count
+    end
+
     # findNoteCounts returns
     #   Evernote::EDAM::NoteStore::NoteCollectionCounts
     #     notebookCounts:{"xxxxxxxx-...xxx": 10, ...},
     #     tagCounts:{"xxxxxx-...xxxx": 1, ..."}
-    def total_count
+    def get_total_count
       counts = core.notestore.findNoteCounts(core.token, NoteFilter.build(options), false)
       counts.notebookCounts.reduce(0){|sum, pair| sum += pair.last }
     end
