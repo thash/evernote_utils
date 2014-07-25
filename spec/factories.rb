1ffd24e2541a9c83feabf9c115873bc0417f3633
memerelics <takuya21hashimoto@gmail.com>
2014-07-25 19:15:10 +0900

    test coverage 70% over

---
+++ spec/factories.rb
@@ -0,0 +1,36 @@
+FactoryGirl.define do
+
+  factory :core, class: ENUtils::Core do
+    token 'hogehoge'
+    notestore 'hoge'
+    initialize_with { new(token, false) }
+  end
+
+  factory :notestore, class: Evernote::EDAM::NoteStore::NoteStore::Client do
+  end
+
+  factory :edam_note, class: Evernote::EDAM::Type::Note do
+    guid 'f1df2a4d-5852-4cb6-82f7-6240ee4e2b5c'
+    title 'title'
+    content nil
+    contentHash 'eeeeeeee6bxxxxxxxxxxxxxxxa889ca7'
+    contentLength 2246
+    created 1266881336000
+    updated 1266881347000
+    active true
+    updateSequenceNum 2653
+    notebookGuid '4xxxxxda-xxxx-xxxx-xxxx-zzzzzzzzzzzz'
+    attributes nil
+  end
+
+  factory :edam_notebook, class: Evernote::EDAM::Type::Notebook do
+    guid 'afa4ba59-xxxx-42ed-xxxx-zzzzzzzzzzzz'
+    name 'Books'
+    updateSequenceNum 24108
+    defaultNotebook false
+    serviceCreated 1297607548000
+    serviceUpdated 1387262389000
+    restrictions nil
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,11 @@ Coveralls.wear!
 
 require 'rubygems'
 require 'evernote_utils'
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
+
+FactoryGirl.definition_file_paths << Pathname.new("../factories.rb")
+FactoryGirl.find_definitions # required
